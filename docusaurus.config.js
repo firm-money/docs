35baa689a7598cb9bf2740417042bc5c3823b559
Joseph <9449596+cupOJoseph@users.noreply.github.com>
2025-12-16 11:31:13 -0500

    Update site URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://docs.must.finance",
+  url: "https://docs.usf.money",
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: "/",
 
